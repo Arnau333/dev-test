476bc972a1789b414e766ad06e76ca95aba3d3d0
Arnau <arnacacu@gmail.com>
2022-09-15 11:26:31 +0200

    pequeño refactoring

--- public/js/main.js
+++ public/js/main.js
@@ -1,57 +1,36 @@
 $(document).ready(function () {
 
 $(`#mainform`).submit(function (e) {
-    e.preventDefault();
-  if ($(`#entrada`).val() == "" || $(`#salida`).val() == "" || $(`#huespedes`).val() == ""){
-    $(`#alerterror`).show();
-  } else{
-    let entrada = $(`#entrada`).val();
-    entrada=entrada.replace("-", "");
-    entrada=entrada.replace("-", "");
+e.preventDefault();
+if ($(`#entrada`).val() == "" || $(`#salida`).val() == "" || $(`#huespedes`).val() == ""){
+  $(`#alerterror`).show();
+} else{
+let entrada = $(`#entrada`).val();
+      entrada=entrada.replace("-", "");
+      entrada=entrada.replace("-", "");
     let salida = $(`#salida`).val();
-    salida=salida.replace("-", "");
-    salida=salida.replace("-", "");
-
+      salida=salida.replace("-", "");
+      salida=salida.replace("-", "");
     let huespedes = $(`#huespedes`).val();
-
       $.ajax({
         url: "https://cmspro.h10hotels.com/_vti_bin/H10_WebServices/H10_Booking.svc/GetRoomPrices/HAL/3082?&fe="+entrada+"&fs="+salida+"&h=HAL&d=Madrid&cp=&c=WEBH10&sc=WEBH10&nh="+huespedes+"&h1Ad=2&h1ni=0&ip=62.174.217.38&u=&agenciacode=&agenciauser=&agenciapassword=&_=1663190941002",
         data: $('#mainform').serialize(),
         type: 'GET',
             success: function(result){
-              $("#resultado").html(result);
-
-
                     $.ajax({
                       url: "https://cmspro.h10hotels.com/_vti_bin/H10_WebServices/H10_Booking.svc/GetBudget/"+result+"/0/3082/undefined?_=1663190941003",
                       data: $('#mainform').serialize(),
                       type: 'GET',
-                          success: function(resultcall2){
-
-                      let index = 0;
-
+                      success: function(resultcall2){
+                       let index = 0;
                       do {
                         $("#resultado").html($("#resultado").html()+"<br><h2><b>"+resultcall2.RoomTypes[index]['Key']+"</h2></b> |Services: <p class='hotelservice'>"+JSON.stringify(resultcall2.RoomTypesInfo[index]));
                         index++;
-                      } while (resultcall2.RoomTypes[index]);
-                 
-                        
-                        
+                      } while (resultcall2.RoomTypes[index]);               
                       }
-                      
-                          
                       });
-
             }
        });
-
-
-
   }
-
 });
-
-
-
-
 });
